e08f4cd959a2b062b32865605868258a3c36dfa9
HEE-JE <windba78@naver.com>
2022-07-11 17:32:53 +0900

    node be programming #2

--- helloweb-ex02/index.js
+++ helloweb-ex02/index.js
@@ -1,9 +1,24 @@
 var http = require('http');
+var path = require('path');
 var express = require('express');
+
+var mainRouter = require('./routes/main');
+var userRouter = require('./routes/user');
 
 var port = 9090;
 // setup application
-var application = express();
+var application = express()
+  // 1. static resources
+  .use(express.static(path.join(__dirname, '/assets')))
+  // 2.  body parser
+  .use(express.urlencoded({ extended: true })) // application/x-www-form-urlencoded
+  .use(express.json()) // application/json
+  // 3. view engine
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  // 4. request router
+  .use('/', mainRouter)
+  .use('/user', userRouter);
 
 // server start
 http
